17d91004d53904a984c170b3ab95925604eed449
Nguyen <lynguyen@amazon.com>
2017-08-01 09:43:44 -0700

    Call AddVersion.py in docker

--- docs/build_version_doc/build_doc.sh
+++ docs/build_version_doc/build_doc.sh
@@ -38,9 +38,9 @@ then
     make docs || exit 1
     echo -e "$latest_tag\n$(cat $tag_list_file)" > "$tag_list_file"
     cat $tag_list_file
-    cd "docs/build_version_doc"
-    python AddVersion.py --file_path "../_build/html/" --current_version "$latest_tag"
-    cd ../..
+    #cd "docs/build_version_doc"
+    tests/ci_build/ci_build.sh docs cd docs/build_version_doc && python AddVersion.py --file_path "../_build/html/" --current_version "$latest_tag"
+    #cd ../..
     cp -a "docs/_build/html/." "$local_build"
     cp $tag_list_file "$local_build/tag.txt"
     rm -rf "$web_folder/.git"
